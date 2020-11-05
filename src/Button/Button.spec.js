c4f3420ec95956dd7e43356b703506efca035491
Dan Jackson <danjackson@hastingsdirect.com>
2020-11-05 14:31:48 +0000

    Snapshot testing added to the Button component test

--- src/Button/Button.spec.js
+++ src/Button/Button.spec.js
@@ -4,6 +4,8 @@ import Button from './Button'
 
 describe('<Button/>', () => {
     it('should render without throwing an error', () => {
-        expect(shallow(<Button/>).find('.a-btn').exists()).toBe(true)
+    	const wrapper = shallow(<Button/>)
+        expect(wrapper.find('.a-btn').exists()).toBe(true)
+        expect(wrapper).toMatchSnapshot()
     })
 })
