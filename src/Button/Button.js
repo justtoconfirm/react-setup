109005761be76df99f822ba7f1255fb94544f6f6
Dan Jackson <danjackson@hastingsdirect.com>
2020-11-06 08:57:18 +0000

    Remove brackets around single props parameter in the Button component

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
 
-const Button = (props) => {
+const Button = props => {
   const {
     type = 'button',
     url = '',
