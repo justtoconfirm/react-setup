98b3f03b5e0cfad8e852ea5bf21a0d013ac33dfc
Dan Jackson <danjackson@hastingsdirect.com>
2020-11-12 14:42:39 +0000

    Update to story reference for the Button component

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,7 +2,7 @@ import { configure } from '@storybook/react'
 
 function loadStories() {
   // You can require as many stories as you need.
-  require('./../src/Button/storybook/Button.stories.js')
+  require('./../src/components/atoms/Button/storybook/Button.stories.js')
 }
 
 configure(loadStories, module)
